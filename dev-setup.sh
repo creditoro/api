079514907ea40186772cf47c636f8b515ecba16a
Simon Jørgensen <55147377+Joergenson@users.noreply.github.com>
2020-05-08 14:22:26 +0200

    Update dev-setup.sh

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,8 +1,7 @@
 export POSTGRES_USER=postgres
 export POSTGRES_DB=creditoro
 export POSTGRES_HOST=localhost
-export POSTGRES_PASSWORD=199109
+export POSTGRES_PASSWORD=postgres
 export POSTGRES_PORT=5432
 export APP_CONFIG=DEV
 export FLASK_APP=app.py
-
