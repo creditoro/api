2c933fbbe2f5ad90756cdf6355ae25d70105fd8b
Kristian Nymann Jakobsen <kristian@nymann.dev>
2020-05-20 12:23:40 +0200

    requirements txt for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@ import os
 import sys
 sys.path.append(os.path.abspath('../../src'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'Creditoro API'
@@ -23,7 +22,6 @@ author = 'Kristian Nymann Jakobsen, Simon Jørgensen, Jakob Rasmusen, Kevin Pete
 
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -40,13 +38,15 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [
+    "_themes",
+]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
